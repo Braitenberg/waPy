1c060ba821502cd998256e9b691dbf7c84ef5f94
tristankjeld <tristankjeld@gmail.com>
2019-10-18 09:23:27 +0200

    minor changes

--- wappi.py
+++ wappi.py
@@ -6,53 +6,23 @@ from selenium.webdriver.common.by import By
 import time
 import sys
 
-class WappiWebInterface:
-    webDrivers = {
-        "firefox": webdriver.Firefox,
-        "chrome": webdriver.Chrome
-    }
-
-    def __init__(self, webDriver, parentClient):
-        if self.webDrivers[webDriver]:
-            self.webDriver = webDriver
-        else:
-            error_string = 'Could not find webdriver %s' % webDriver
-            raise Exception(error_string)
-
-        self.parentClient = parentClient
-
-    def commandListener(self):
-        # Listen for command
-        driver = self.webDriver()
-        driver.get("https://web.whatsapp.com/")
-
-        text = 0
-        return self.parentClient.execute(text)
-
 class WappiClient:
     def __init__(self, webDriver, name):
-        self.webInterface = WappiWebInterface(webDriver=webDriver, parentClient=self)
-        self.collections = []
+        # todo: add webdriver
         self.name = name
 
-    def execute(self, functionName, args=[]):
-        for collection in self.collections:
-            if collection.functionName:
-                return collection.executeFunction(functionName, args)
+    def action(func):
+        # https://realpython.com/primer-on-python-decorators/
+        return 0
 
-    def addCollection(self, collection):
-        return self.collections.append(collection)
 
-class WappiCollection:
-    def executeFunction(functionName, args=[]):
-        print(f'executing function {functionName} with args {args}...')
-        return functionName(args)
-    pass
+wappi = WappiClient(webDriver='firefox', name='john')
 
-class CustomCollection(WappiCollection):
-    def helloWorld(self, userName):
-        return f"hello world, {userName}" 
+@wappi.action
+def hello_world(name):
+    print(f'Hello, {name}!')
 
-collection = CustomCollection()
-wappi = WappiClient('firefox', 'custom')
-wappi.addCollection(collection)
+hello_world('tristan')
+
+
+
